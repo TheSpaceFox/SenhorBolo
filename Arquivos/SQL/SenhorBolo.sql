d4a4521b83e3c1f448fa965ad1edc4122add2e3a
FeFeLanches <felipe.granja@etec.sp.gov.br>
2020-11-13 13:27:43 -0300

    Update SenhorBolo.sql

--- Arquivos/SQL/SenhorBolo.sql
+++ Arquivos/SQL/SenhorBolo.sql
@@ -45,3 +45,34 @@ CREATE TABLE tblFornecedor(
 	enderecoFor VARCHAR(50),
 	cidade VARCHAR(25)
 )
+
+
+insert into tblProduto values
+(1, 'Abacaxi', 20.00, 8.00),
+(2, 'Ameixa', 20.00, 8.00),
+(3, 'Banana com Canela', 20.00, 8.00),
+(4, 'Banana com Passas', 20.00, 8.00),
+(5, 'Cenoura', 20.00, 8.00),
+(6, 'Cenoura Mesclado', 20.00, 8.00),
+(7, 'Chocolate', 20.00, 8.00),
+(8, 'Coco', 20.00, 8.00),
+(9, 'Formigueiro', 20.00, 8.00),
+(10, 'Fubá', 20.00, 8.00),
+(11, 'Fubá com Erva Doce', 20.00, 8.00),
+(12, 'Fubá com Goiabada', 20.00, 8.00),
+(13, 'Laranja', 20.00, 8.00),
+(14, 'Limão', 20.00, 8.00),
+(15, 'Maçã com Canela', 20.00, 8.00),
+(16, 'Mandioca com Coco', 20.00, 8.00),
+(17, 'Mesclado', 20.00, 8.00),
+(18, 'Milho', 20.00, 8.00),
+(19, 'Paçoca', 20.00, 8.00),
+(20, 'Brigadeiro', 110.00, 60.00),
+(21, 'Cocolate com Morango', 120.00, 65.00),
+(22, 'Prestígio', 110.00, 60.00),
+(23, 'Toalha Felpuda', 110.00, 60.00),
+(24, 'Calabresa', 32.00, 15.00),
+(25, 'Escarola com Bacon', 32.00, 15.00),
+(26, 'Pão de Queijo', 32.00, 15.00),
+(27, 'Queijo com Bacon', 32.00, 15.00),
+(28, 'Brigadeiro de Colher', 3.00, 2.50)
